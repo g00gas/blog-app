423bd27399c0190a22b26b1cc1a524a7caa20185
g00gas <noshar951@gmail.com>
2024-03-02 18:20:42 +0100

    infra rework, add check for db

--- backend/internal/middleware/useDatabase.go
+++ backend/internal/middleware/useDatabase.go
@@ -1,12 +1,20 @@
 package middleware
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func UseDatabase(db *pgxpool.Pool) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		err := db.Ping(ctx)
+		if err != nil {
+			fmt.Printf("Cannot connect to database: %s\n", err)
+			os.Exit(1)
+		}
 		ctx.Set("DB", db)
 		ctx.Next()
 	}
---
+++ init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(post_id),
+    author VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO posts (title, content) VALUES ('Hello world!', 'Hello dear world content');
+
+INSERT INTO comments (post_id, author, content) VALUES (1, 'foo@baz.com', 'u suck lol');
